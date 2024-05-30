922d0601524addc3217e8565040e33a1565fb843
italo barros <italobarros.me@gmail.com>
2024-05-30 12:28:07 -0300

    feat: implement layout and routes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Source_Code_Pro } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const SourceCodePro = Source_Code_Pro({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});

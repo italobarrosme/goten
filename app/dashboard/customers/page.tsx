b74ba1f36a3a4fc200b19b684f446736b5acbeed
italo barros <italobarros.me@gmail.com>
2024-05-31 09:17:45 -0300

    feat: tada course learn nextjs

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { SourceCodePro } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+};
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+       <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
